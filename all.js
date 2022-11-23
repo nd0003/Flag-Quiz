26d6274d3034cae74cb0bee950195a31944d3ba3
Neil De Kock <100484177+nd0003@users.noreply.github.com>
2022-11-23 17:49:37 +0100

    Add files via upload

--- all.js
+++ all.js
@@ -1,41 +1,47 @@
-/* Declaring variables */
-let score = document.querySelector(".score span");
-let scoreDiv = document.querySelector(".score");
-let countSpan = document.querySelector(".count span");
-let img = document.querySelector(".img img");
-let imgDiv = document.querySelector(".img");
-let options = document.querySelector(".options ul");
-let list = document.querySelector(".options ul li");
-let finalScore = document.querySelector(".finalScore");
-let currentIndex = 0;
-let correctAnswer = 0;
-let incorrectAnswer = document.querySelector(".score .incorrect span");
-let cAns = document.querySelector(".score .correct span");
+function restart() {
+  window.location = "http://ndquiz.epizy.com/flags/all.html";
+}
 
-/* Making the flags and questions show up on the website */
-function getData() {
-  let req = new XMLHttpRequest();
-  req.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
+function newGame() {
+  window.location = "http://ndquiz.epizy.com/flags/";
+}
+
+let numSpan = document.querySelector(".num span");
+let imgDiv = document.querySelector(".flag-img");
+let flagImg = document.querySelector(".flag img img");
+let options = document.querySelector(".options ul");
+let list = document.querySelectorAll(".options ul li");
+let score = document.querySelector(".score span");
+let scoreDiv = document.querySelector(".score-div");
+let currentIndex = 0;
+let rightAnswer = 0;
+let correctAns = document.querySelector(".score-div .correct span");
+
+function getQ() {
+  let myReq = new XMLHttpRequest();
+  myReq.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
       let questions = JSON.parse(this.responseText);
       let numQ = 251;
       qNum(numQ);
       questions = questions
         .sort(() => Math.random() - Math.random())
-        .slice(0, 251);
+        .slice(0, 10);
 
       addData(questions[currentIndex], numQ);
 
       list.forEach((li) => {
         li.addEventListener("click", () => {
-          let correctAnswer = questions[currentIndex].correctAnswer;
+          let rightAnswer = questions[currentIndex].rightAnswerr;
           li.classList.add("active");
           currentIndex++;
+
           setTimeout(() => {
-            checkAnswer(correctAnswer, numQ);
+            checkAns(rightAnswer, numQ);
           }, 500);
+
           setTimeout(() => {
-            img.src = "";
+            flagImg.src = "";
             li.classList.remove("active");
             li.classList.remove("correct");
             li.classList.remove("incorrect");
@@ -49,19 +55,19 @@ function getData() {
       });
     }
   };
-  req.open("GET", "random.json", true);
-  req.send();
+  myReq.open("GET", "random.json", true);
+  myReq.send();
 }
 
-getData();
+getQ();
 
 function qNum(num) {
-  countSpan.innerHTMl = num;
+  numSpan.innerHTML = num;
 }
 
-function addData(obj, count) {
-  if (currentIndex < count) {
-    img.src = `img/${obj.img}`;
+function addData(obj, numm) {
+  if (currentIndex < numm) {
+    flagImg.src = `img/${obj.img}`;
     list.forEach((li, i) => {
       li.id = `answer_${i + 1}`;
       li.dataset.answer = obj[`options`][i];
@@ -70,7 +76,7 @@ function addData(obj, count) {
   }
 }
 
-function checkAnswer(rAns, count) {
+function checkAns(rAns, numm) {
   let chosenAns;
   for (let i = 0; i < list.length; i++) {
     if (list[i].classList.contains("active")) {
@@ -86,11 +92,11 @@ function checkAnswer(rAns, count) {
   }
 }
 
-function showResults(count) {
-  if (currentIndex === count) {
+function showResults(numm) {
+  if (currentIndex === numm) {
     options.innerHTML = "";
     imgDiv.innerHTML = "";
     scoreDiv.style.display = "block";
-    correctAnswer.innerHTML = cAns;
+    correctAns.innerHTML = rightAnswer;
   }
 }
